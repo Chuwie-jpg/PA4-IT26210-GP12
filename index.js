a5d4b607c9725723838316b360635b5f567db1b6
Vince <jusonvince11@gmail.com>
2024-12-01 18:33:48 +0800

    Fix ESLint permissions in CI workflow

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
+// A simple route
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
